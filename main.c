add06324dd17eddd798017787b78442d95ecca7d
Mateus Medeiros lopes <mmedeiro@c1t3s5.42.rio>
2022-08-09 13:37:34 -0300

    so_long

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <mlx.h>
+
+typedef struct s_data {
+	void *img;
+	char *addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}					t_data;
+
+int main(void)
+{
+	void *mlx;
+	t_data	img;
+
+	mlx = mlx_init();
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+}
