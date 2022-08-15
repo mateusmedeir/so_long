159f6683bff1d7d68f6ca9c4233e26f6d4bc9966
Mateus <mateusmlopestr@gmail.com>
2022-08-15 16:00:54 -0300

    so_long

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(void)
 
 	mlx = mlx_init();
 	ft_screen(&screen, mlx, 1600, 900, "so_long");
-	ft_image(&image, mlx, "block.xpm");
+	ft_image(&image, mlx, "character.xpm");
 	size.x = 0;
 	size.y = 0;
 	mlx_put_image_to_window(mlx, screen.ref, image.ref, size.x, size.y);
