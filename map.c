7b65c495f2e3fa08a1026e2d068446572df1005a
Mateus Medeiros lopes <mmedeiro@c1t3s5.42.rio>
2022-08-16 14:07:54 -0300

    so_long

--- map.c
+++ map.c
@@ -6,22 +6,27 @@ t_block	ft_map(t_long so_long, char *map)
 {
 	t_block	map_block;
 	t_size	position;
-	char	*a;
+	char	*line;
 	int		fd;
 	int		counter;
 
 	fd = open(map, O_RDONLY);
-	a = get_next_line(fd);
+	line = get_next_line(fd);
 	position.x = 0;
 	position.y = 0;
-	while (a[0])
+	while (line[0])
 	{
 		position.y += 1;
 		counter = -1;
-		while (a[++counter])
-			ft_map_block(map_block, position, a);
-		a = get_next_line(fd);
+		while (line[++counter])
+			ft_map_block(map_block, position, line);
+		line = get_next_line(fd);
 	}
 	map.x = counter;
 	return (map);
+}
+
+void	ft_map_block(t_block map, t_size position, char *line)
+{
+	
 }
